4455373fd532b896de15169e609dd4f52aa53c39
Dhaya <sekarsekar5017@gmail.com>
2024-11-24 23:20:22 +0530

    upadte:header

--- client/script.js
+++ client/script.js
@@ -2,4 +2,7 @@ import {io} from "socket.io-client"
 const socket=io("http://localhost:3000")
 socket.on("connect",()=>{
     alert(`Connected to server:${socket.id}`)
+    const p = document.createElement("p")
+    p.innerText="Connected to server:"+socket.id
+    document.querySelector(".chat-header").appendChild(p)
 })
