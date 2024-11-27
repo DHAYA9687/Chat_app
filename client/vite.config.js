8c04b4bb8960bde2f1a47b2062f94c3b757cc2d6
Dhaya <sekarsekar5017@gmail.com>
2024-11-27 15:19:57 +0530

    vite:updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,12 @@ export default {
     build: {
       outDir: 'dist', // Default; change only if you want a different folder
     },
+    resolve: {
+        alias: {
+            'socket.io-client': 'socket.io-client/dist/socket.io.esm.js',
+        },
+    },
+      
+      
   };
   
